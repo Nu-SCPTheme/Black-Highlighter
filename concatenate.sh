24e15c5780f40149b5a13c633ac2c032f57e0646
Ross Williams <rossjrw@gmail.com>
2019-02-15 04:40:50 +0000

    Update build for image extraction

--- concatenate.sh
+++ concatenate.sh
@@ -34,7 +34,7 @@ cp styles/main.css temp.css
 
 # Scrap the @import rules
 echo "Removing @imports..."
-sed -i '/^@import.*\.css.*$/d' temp.css || error "@import removal failed"
+sed -i "/^@import.*\.css.*$/d" temp.css || error "@import removal failed"
 
 # Append this file to root.css
 echo "Prepending root.css..."
@@ -49,15 +49,35 @@ cat styles/!(main|root|normalize|overwrite-main).css >> temp.css
 echo "Created nuscp.css"
 cp temp.css stable/styles/nuscp.css
 
+# Extract inline images to array
+IFS='>'
+IMAGES=($(awk -F '>' '/data:image/ && match($0,/\".*\"/){val=val?val ">" substr($0,RSTART+1,RLENGTH-2):substr($0,RSTART+1,RLENGTH-2)} END{print val}' temp.css))
+echo "Found ${#IMAGES[@]} images to extract"
+echo "Extracting images..."
+count=0
+for i in "${IMAGES[@]}"; do
+	#echo "IMAGES[$count]=$i"
+	sed -i -e "s>$i>!!MARKER$count>g" temp.css || error "Bad sed"
+	count=$((count + 1))
+done
+
 # Minify
 echo "Compressing..."
 yui-compressor --type css -o temp.css temp.css
 
 # Correct compression errors
 # "-(" -> "- ("
-sed -i -e 's/-(/- (/g' temp.css
+sed -i -e "s/-(/- (/g" temp.css
 # "Xrem+Yrem" -> "Xrem + Yrem"
-sed -i -E 's/(calc\([^\+]*?\S)(\+)(\S[^\+]*?\)[;}])/\1 + \3/g' temp.css
+sed -i -E "s/(calc\([^\+]*?\S)(\+)(\S[^\+]*?\)[;}])/\1 + \3/g" temp.css
+
+# Reinsert extracted images
+echo "Reinserting extracted images..."
+count=0
+for i in "${IMAGES[@]}"; do
+	sed -i -E "s>!!MARKER$count>$i>g" temp.css
+	count=$((count + 1))
+done
 
 # Move the minified file to final destination
 mv temp.css stable/styles/nuscp.min.css
