f0c4ca10d76ad641b593f7802083cb5a22fcb1d2
Woed E. Naz <leminnes@gmail.com>
2019-03-28 10:00:22 -0400

    Fixing Calc in Root and adding Dust Jacket Theme
    Fixing Calc in Root and adding Dust Jacket Theme

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,62 @@
 const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
+const postcss = require('gulp-postcss');
+const cssvariables = require('postcss-css-variables');
+const unprefix = require("postcss-unprefix");
+const mergerules = require("postcss-merge-rules");
+const stylefmt = require('gulp-stylefmt');
+const stylelint = require("stylelint");
 
 gulp.task('default', () =>
-	gulp.src('styles/*.css')
+	gulp.src('themes/fallbacks/*.css')
 		.pipe(sourcemaps.init())
 			.pipe(autoprefixer({
                 browsers: ['last 50 versions'],
                 cascade: true
             }))
-		.pipe(sourcemaps.write('dist/'))
-		.pipe(gulp.dest('styles/'))
+		.pipe(gulp.dest('themes/fallbacks'))
 );
+
+
+gulp.task('css', () => 
+	gulp.src('themes/dustjacket-theme.css').pipe(
+		postcss([
+			cssvariables({
+				preserve: true
+			})
+		])
+	).pipe(
+	gulp.dest('themes/fallbacks')
+));
+
+gulp.task('rmv', () => 
+	gulp.src('themes/*.css').pipe(
+		postcss([
+			unprefix()
+		])
+	).pipe(
+	gulp.dest('themes/fallbacks')
+));
+
+
+gulp.task('merge', () => 
+	gulp.src('themes/fallbacks/*.css').pipe(
+		postcss([
+			mergerules()
+		])
+	).pipe(
+	gulp.dest('themes/fallbacks')
+));
+
+gulp.task('stylefmt', () => 
+	gulp.src('styles/*.css').pipe(
+		stylefmt())
+    .pipe(gulp.dest('styles')
+));
+
+gulp.task('stylefmt-themes', () => 
+	gulp.src('themes/*.css').pipe(
+		stylefmt())
+    .pipe(gulp.dest('themes')
+));
