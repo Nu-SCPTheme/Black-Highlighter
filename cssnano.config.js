d294a848484fdcc484f7d10873bc36d38846f56d
Woed E. Naz <leminnes@gmail.com>
2019-07-05 10:36:41 -0400

    Adding some cssnano options

--- cssnano.config.js
+++ cssnano.config.js
@@ -4,5 +4,9 @@ module.exports = defaultPreset({
     discardComments: {
         removeAll: true,
     },
+    cssDeclarationSorter: {
+        order: "concentric-css"
+    },
     normalizeCharset: false,
+    reduceIdents: false
 });
