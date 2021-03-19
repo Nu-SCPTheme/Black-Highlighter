0054b69c1e9f94532f617b68c636a552aeecc2b7
Ammon Smith <ammon.i.smith@gmail.com>
2021-03-19 03:37:12 -0400

    Format cssnano config.

--- build/cssnano.config.js
+++ build/cssnano.config.js
@@ -1,12 +1,12 @@
-const defaultPreset = require("cssnano-preset-default");
+const defaultPreset = require('cssnano-preset-default');
 
 module.exports = defaultPreset({
-    discardComments: {
-        removeAll: true,
-    },
-    cssDeclarationSorter: {
-        order: "concentric-css"
-    },
-    normalizeCharset: false,
-    reduceIdents: false
+  discardComments: {
+    removeAll: true,
+  },
+  cssDeclarationSorter: {
+    order: 'concentric-css',
+  },
+  normalizeCharset: false,
+  reduceIdents: false,
 });
