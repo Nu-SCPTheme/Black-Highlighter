87262d88196f0fad7131dc5d10ec776c625f436e
Ammon Smith <ammon.i.smith@gmail.com>
2021-03-22 19:47:58 -0400

    Reorder build/int.mk

--- build/int.mk
+++ build/int.mk
@@ -1,20 +1,11 @@
-
-# Dynamic patching and building for any INT directories present
+# Static declaration of INT branches available
 INT_BRANCHES  := \
 	cn \
 	test
 
-INT_SOURCES   := \
-	$(INT_SOURCES_CN) \
-	$(INT_SOURCES_TEST)
-
-INT_OUTPUTS   := \
-	$(INT_OUTPUTS_CN) \
-	$(INT_OUTPUTS_TEST)
-
 INT_DIRS      := $(patsubst src/css/int/%,%,$(wildcard src/css/int/*))
 
-# Assert that the directories in INT match the constant list.
+# Assert that the directories in INT match the constant list
 ifneq ($(INT_BRANCHES), $(INT_DIRS))
 	$(error Declared list of branches doesn't match directory!)
 endif
@@ -22,3 +13,12 @@ endif
 # See build/int-template.mk for the template and why this code has to be duplicated
 include build/int-cn.mk
 include build/int-test.mk
+
+# Sum variables
+INT_SOURCES   := \
+	$(INT_SOURCES_CN) \
+	$(INT_SOURCES_TEST)
+
+INT_OUTPUTS   := \
+	$(INT_OUTPUTS_CN) \
+	$(INT_OUTPUTS_TEST)
