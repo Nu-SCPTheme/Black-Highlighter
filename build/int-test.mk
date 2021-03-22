37dd32e3ceb9304f3b3203c6f6919500cea23294
Ammon Smith <ammon.i.smith@gmail.com>
2021-03-22 19:57:17 -0400

    Fix int test rule.

--- build/int-test.mk
+++ build/int-test.mk
@@ -8,7 +8,7 @@ INT_OUTPUTS_TEST := \
 	dist/css/int/test/min/normalize.min.css
 
 dist/css/int/test/black-highlighter.css dist/css/int/test/normalize.css: $(INT_SOURCES_TEST)
-	build/int-patch-and-merge.sh cn
+	build/int-patch-and-merge.sh test
 
 dist/css/int/test/min/black-highlighter.min.css: dist/css/int/test/black-highlighter.css
 	npm run postcss -- --config build/css-minify -o $@ $<
