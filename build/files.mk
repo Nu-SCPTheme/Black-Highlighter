7682822d2c18ad6ef2e3b367e164df777dbb7c41
Ammon Smith <ammon.i.smith@gmail.com>
2021-07-14 08:40:30 -0400

    Patch scp-test-page files for prod.

--- build/files.mk
+++ build/files.mk
@@ -1,23 +1,15 @@
 # Variables
 FILES_SOURCES := \
-	scp-test-page/scp-test-page.html \
-	scp-test-page/scp-test-page.js \
 	src/misc/domicile.html \
 	src/root/index.html \
 	src/root/error.html
 FILES_OUTPUTS := \
-	dist/scp-test-page.html \
-	dist/scp-test-page.js \
 	dist/spherical/domicile.html \
 	dist/index.html \
 	dist/error.html
 
 # Dummy rules
 package-lock.json:
-
-# Static files
-dist/scp-test-page.%: scp-test-page/scp-test-page.%
-	install -D -m644 $< $@
 
 dist/spherical/domicile.html: src/misc/domicile.html
 	install -D -m644 $< $@
