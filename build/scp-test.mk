e64957f9ecfcc35a71892ff98671a94b2ba182cd
Ammon Smith <ammon.i.smith@gmail.com>
2021-07-14 08:49:20 -0400

    Fix make rules.

--- build/scp-test.mk
+++ build/scp-test.mk
@@ -7,11 +7,11 @@ SCP_TEST_OUTPUTS := \
 	dist/scp-test-page.js
 
 # Copy and inline-patch files
-dist/scp-test-page.html: scp-test-page/scp-test-page.html
+dist/scp-test-page.html: src/scp-test-page.html
 	install -D -m644 $< $@
 	sed -i 's|\./scp-test-page.js|/scp-test-page.js|' $@
 
-dist/scp-test-page.js: scp-test-page/scp-test-page.js
+dist/scp-test-page.js: src/scp-test-page.js
 	install -D -m644 $< $@
 	sed -i 's|\.\./src/css|/css|' $@
 
