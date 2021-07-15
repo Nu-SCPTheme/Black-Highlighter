fe40053a7795c67e10f692ce61ecb9b066692773
woedenaz <leminnes@gmail.com>
2021-07-15 00:58:40 -0400

    More cleaning up + Common./Int Site Compatability

--- src/scp-test-page.js
+++ src/scp-test-page.js
@@ -1,20 +1,29 @@
 /* jshint esversion: 8 */
-$(function() {
+$(function () {
+
 	let styleSheets = [];
 	let bhlSheets = "bhl";
 
-	/* BHL Stylesheets */
+	var bhlMinDetect;
+	var bhlDetect;
+
+	//BHL Stylesheets
 	styleSheets = [
 		"./css/normalize.css",
 		"./css/black-highlighter.css"
 	];
 
+	//Async Wait Function
+	function wait(ms) {
+		return new Promise(r => setTimeout(r, ms));
+	}
+
+
 	//Polyfill for DOMParser
 	(function (DOMParser) {
 		"use strict";
 
-		var
-			DOMParser_proto = DOMParser.prototype,
+		var DOMParser_proto = DOMParser.prototype,
 			real_parseFromString = DOMParser_proto.parseFromString;
 
 		// Firefox/Opera/IE throw errors on unsupported types
@@ -47,15 +56,15 @@ $(function() {
 		let result = '';
 		let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 		let charactersLength = characters.length;
-		for ( let i = 0; i < length; i++ ) {
-			result += characters.charAt(Math.floor(Math.random() * 
-	 		charactersLength));
-	   }
-	   return result;
+		for (let i = 0; i < length; i++) {
+			result += characters.charAt(Math.floor(Math.random() *
+				charactersLength));
+		}
+		return result;
 	}
 
 	//Function to inject stylesheets
-	let changeStyleSheet = (cssFile, cssId) => {
+	let changeStyleSheet = async (cssFile, cssId) => {
 		let cssIdSelect = "#" + cssId;
 		if ($(cssIdSelect) && cssFile.length == 1) {
 			$(cssIdSelect).href = cssFile;
@@ -89,34 +98,81 @@ $(function() {
 
 	//Use codetabs.com to pull source of page & Apply to local page
 	let scpwikiurl = getUrlParameter("url");
-	let getNewElems = async () => {	
+
+	let sitePara = getUrlParameter("site");
+	let siteURL;
+	switch (sitePara) {
+		case "int":
+			siteURL = "scp-int";
+			break;
+		case "ru":
+			siteURL = "scp-ru";
+			break;
+		case "ko":
+			siteURL = "scpko";
+			break;
+		case "cn":
+			siteURL = "scp-wiki-cn";
+			break;
+		case "fr":
+			siteURL = "fondationscp";
+			break;
+		case "pl":
+			siteURL = "scp-pl";
+			break;
+		case "es":
+			siteURL = "lafundacionscp";
+			break;
+		case "th":
+			siteURL = "scp-th";
+			break;
+		case "jp":
+			siteURL = "scp-jp";
+			break;
+		case "de":
+			siteURL = "scp-wiki-de";
+			break;
+		case "it":
+			siteURL = "fondazionescp";
+			break;
+		case "ua":
+			siteURL = "scp-ukrainian";
+			break;
+		case "pt":
+			siteURL = "scp-pt-br";
+			break;
+		case "zh-tr":
+			siteURL = "scp-zh-tr";
+			break;
+		default:
+			siteURL = "scp-wiki";
+	}
+	let getNewElems = async () => {
 		$.ajax({
-			url:`https://api.codetabs.com/v1/proxy/?quest=https://scp-wiki.wikidot.com/${scpwikiurl}?${randomString(5)}`,
-			type:'GET',
-			success: function(data){							
-				let href = "href=\"https://scp-wiki.wikidot.com/";
-				let src = "src=\"https://scp-wiki.wikidot.com/";
+			url: `https://api.codetabs.com/v1/proxy/?quest=https://${siteURL}.wikidot.com/${scpwikiurl}?${randomString(5)}`,
+			type: 'GET',
+			success: function (data) {
+				let href = `href=\"https://${siteURL}.wikidot.com/`;
+				let src = `src=\"https://${siteURL}.wikidot.com/`;
 				let dp = new DOMParser();
 				let doc = dp.parseFromString(data
 					.replace(/(href="\/)/g, href)
 					.replace(/(src="\/)/g, src)
-					.replace(/(http:\/\/)/g, "https://"), 
+					.replace(/(http:\/\/)/g, "https://"),
 					"text/html");
-				console.log(`doc> ${doc.documentElement.innerHTML}`)		
 				let newHeadContents = doc.getElementsByTagName("head")[0].innerHTML;
 				let newHead = doc.getElementsByTagName("head")[0];
 				let newBody = doc.getElementsByTagName("body")[0];
-				let bhlMinDetect = String(newHeadContents).indexOf("black-highlighter.min.css");
-				let bhlDetect = String(newHeadContents).indexOf("black-highlighter.css");
+				bhlMinDetect = String(newHeadContents).indexOf("black-highlighter.min.css");
+				bhlDetect = String(newHeadContents).indexOf("black-highlighter.css");
 				let iframesReplace = document.getElementsByTagName("iframe");
 				document.getElementsByTagName("head")[0].appendChild(newHead).after("\n");
-				document.getElementsByTagName("body")[0].appendChild(newBody);					
-				if (bhlDetect == -1 && bhlMinDetect == -1 ) {
-					changeStyleSheet(styleSheets,bhlSheets);				
-				}		
-				$(iframesReplace).each(function(idx,el){
+				document.getElementsByTagName("body")[0].appendChild(newBody);
+
+				$(iframesReplace).each(function (idx, el) {
 					el.src = `https://api.codetabs.com/v1/proxy/?quest=${el.src}`
 				});
+				return bhlMinDetect, bhlDetect;
 			}
 		});
 	};
@@ -130,27 +186,27 @@ $(function() {
 			let lTime = 500;
 			let sTime = 500;
 			let bTime = 2000;
-			$(links).each(function(idx,el){
-				setTimeout( function(){
+			$(links).each(function (idx, el) {
+				setTimeout(function () {
 					let link = document.createElement("link");
 					let lHref = links[idx].getAttribute("href");
 					let lRel = links[idx].getAttribute("rel");
 					let lTyp = links[idx].getAttribute("type");
 					if (lTyp) {
-						link.type = lTyp;	
+						link.type = lTyp;
 					}
 					if (lHref) {
 						link.href = `${lHref}?${randomString(5)}`;
 					}
 					if (lRel) {
-						link.rel = lRel;					
+						link.rel = lRel;
 					}
-					document.getElementsByTagName("head")[0].appendChild(link);
+					document.getElementsByTagName("head")[0].prepend(link);
 				}, lTime)
 				lTime += 500;
-			});			
-			$(scripts).each(function(idx,el){
-				setTimeout( function(){
+			});
+			$(scripts).each(function (idx, el) {
+				setTimeout(function () {
 					let script = document.createElement("script");
 					let pSrc = scripts[idx].getAttribute("src");
 					let pTyp = scripts[idx].getAttribute("type");
@@ -159,18 +215,18 @@ $(function() {
 						script.type = pTyp;
 						if (pSrc) {
 							script.src = `${pSrc}?${randomString(5)}`;
-							document.getElementsByTagName("head")[0].appendChild(script);
+							document.getElementsByTagName("head")[0].prepend(script);
 						}
 						if (pTxt) {
 							script.innerHTML = pTxt;
-							document.getElementsByTagName("head")[0].appendChild(script);
-						}			
-					}	
+							document.getElementsByTagName("head")[0].prepend(script);
+						}
+					}
 				}, sTime)
-				sTime += 500;		
+				sTime += 500;
 			});
-			$(bScripts).each(function(idx,el){
-				setTimeout( function(){
+			$(bScripts).each(function (idx, el) {
+				setTimeout(function () {
 					let script = document.createElement("script");
 					let bSrc = bScripts[idx].getAttribute("src");
 					let bTyp = bScripts[idx].getAttribute("type");
@@ -179,21 +235,31 @@ $(function() {
 						script.type = bTyp;
 						if (bSrc) {
 							script.src = `${bSrc}?${randomString(5)}`;
-							document.getElementsByTagName("body")[0].appendChild(script);
+							document.getElementsByTagName("body")[0].prepend(script);
 						}
 						if (bTxt) {
 							script.innerHTML = bTxt;
-							document.getElementsByTagName("body")[0].appendChild(script);
-						}			
-					}	
+							document.getElementsByTagName("body")[0].prepend(script);
+						}
+					}
 				}, bTime)
-				bTime += 500;		
+				bTime += 500;
 			});
-		} catch(e) {
+
+		} catch (e) {
 			console.log(e);
 		}
-	};	
+	};
 
-	getNewElems();
-	setTimeout(function(){ refreshScripts() }, 1000);
+	let finalInit = async () => {
+		await getNewElems();
+		await wait(1000);
+		await refreshScripts();
+		console.log(`bhlDetect: ${bhlDetect} // bhlMinDetect: ${bhlMinDetect}`);
+		if (bhlDetect == -1 && bhlMinDetect == -1) {
+			await changeStyleSheet(styleSheets, bhlSheets);
+		}
+	};
+
+	finalInit();
 });
