bea8c4fbce6e8350ad181a181e2aa1bdcb53d804
woedenaz <leminnes@gmail.com>
2021-07-18 19:39:58 -0400

    User/Search Bar/Button Edits

--- build/fonts.mk
+++ build/fonts.mk
@@ -3,10 +3,22 @@ FONTS_SOURCES := $(wildcard src/fonts/*)
 FONTS_OUTPUTS := $(patsubst src/fonts/%,dist/fonts/%,$(FONTS_SOURCES))
 
 # Dummy rules
+dist/fonts/%.ttf: src/fonts/%.ttf
+	build/install.sh 644 $< $@
+
 dist/fonts/%.woff: src/fonts/%.woff
 	build/install.sh 644 $< $@
 
 dist/fonts/%.woff2: src/fonts/%.woff2
+	build/install.sh 644 $< $@
+
+dist/fonts/%.eot: src/fonts/%.eot
+	build/install.sh 644 $< $@
+
+dist/fonts/%.otf: src/fonts/%.otf
+	build/install.sh 644 $< $@
+
+dist/fonts/%.svg: src/fonts/%.svg
 	build/install.sh 644 $< $@
 
 dist/fonts/%.css: src/fonts/%.css
