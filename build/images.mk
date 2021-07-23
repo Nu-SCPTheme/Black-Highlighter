906f8efc1c849a01ff68433ca5905a4c9aa77c01
Ammon Smith <ammon.i.smith@gmail.com>
2021-07-23 10:02:52 -0400

    Add separate handling for icon files.
    These are not to be optimized and should be copied directly.

--- build/images.mk
+++ build/images.mk
@@ -1,8 +1,12 @@
-# Variables
-IMAGE_SOURCES := $(wildcard src/img/*)
-IMAGE_OUTPUTS := $(patsubst src/img/%,dist/img/%,$(IMAGE_SOURCES))
+# Image files to run the optimizer on
+IMAGE_OPTIMIZE_SOURCES := $(wildcard src/img/*.*)
+IMAGE_OPTIMIZE_OUTPUTS := $(patsubst src/img/%,dist/img/%,$(IMAGE_OPTIMIZE_SOURCES))
 
-# Image optimization
+# Image files to copy as-is
+IMAGE_PLAIN_SOURCES    := $(wildcard src/img/page-toolbar-icons/* src/img/text-editor-icons/*)
+IMAGE_PLAIN_OUTPUTS    := $(patsubst src/img/%,dist/img/%,$(IMAGE_PLAIN_SOURCES))
+
+# Image optimization rules
 dist/img/%.gif: src/img/%.gif node_modules
 	npm run optimize -- gif $< $@
 
@@ -11,3 +15,7 @@ dist/img/%.png: src/img/%.png node_modules
 
 dist/img/%.svg: src/img/%.svg node_modules
 	npm run optimize -- svg $< $@
+
+# Image copy rule
+dist/img/%: src/img/%
+	build/install.sh 644 $< $@
