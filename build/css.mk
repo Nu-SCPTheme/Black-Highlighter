770a6cd2f9cc6d644cd2e8ee193d0b82de60b886
woedenaz <leminnes@gmail.com>
2024-01-07 15:45:52 -0500

    Removing unnecessary image size function

--- build/css.mk
+++ build/css.mk
@@ -12,13 +12,13 @@ dist/css/parts/%: src/css/parts/%
 		build/install.sh 644 $< $@
 
 # CSS rules
-dist/css/min/black-highlighter.min.css: src/css/black-highlighter.css $(CSS_SOURCES) | node_modules
+dist/css/min/black-highlighter.min.css: src/css/black-highlighter.css $(CSS_SOURCES) node_modules
 		build/install.sh 644 $< dist/css/black-highlighter-imports.css
 		bun postcss dist/css/black-highlighter-imports.css --config build --env development -o dist/css/black-highlighter.css
 		bun postcss dist/css/black-highlighter-imports.css --config build --env production -o $@
 		touch $@
 
-dist/css/min/normalize.min.css: src/css/normalize.css | node_modules
+dist/css/min/normalize.min.css: src/css/normalize.css node_modules
 		build/install.sh 644 $< dist/css/normalize.css
 		bun postcss dist/css/normalize.css --config build --env development -r
 		bun postcss dist/css/normalize.css --config build --env production -o $@
